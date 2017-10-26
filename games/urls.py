72b3d55232befdb6a652c6ab987e26f9075f90bc
James Cox <j-cox03@hotmail.com>
2017-10-26 15:26:56 +0100

    made index and create routes for games

--- games/urls.py
+++ games/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^', views.index, name="games-index"),
+    url(r'^', views.games, name="games-index"),
 ]
--- games/views.py
+++ games/views.py
@@ -1,7 +1,32 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.http import JsonResponse
+from .models import Game
 
-def index(request):
-    return JsonResponse({"out": "result"})
+from django.http import HttpResponse
+from django.http import JsonResponse
+import json
+
+from django.views.decorators.csrf import csrf_exempt
+from django.core import serializers
+
+
+@csrf_exempt
+def games(request):
+    if(request.method == 'GET'):
+        return index_route(request)
+    if(request.method == 'POST'):
+        return create_route(request)
+
+def index_route(request):
+    payload = serializers.serialize('json', Game.objects.all())
+    return HttpResponse(payload, 'application/json')
+
+def create_route(request):
+    data=json.loads(request.body)
+    new_game = Game(
+        title=data["title"], body=data["body"],
+        time_limit_seconds=data["time_limit_seconds"], difficulty=Game.EASY
+    )
+    new_game.save()
+    return JsonResponse({"save": new_game.pk != None })
