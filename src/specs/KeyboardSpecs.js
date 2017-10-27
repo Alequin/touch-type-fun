86eed89b01e74dc4f1a7a4f02e1192cc812cb63e
James Cox <j-cox03@hotmail.com>
2017-10-27 08:02:47 +0100

    set up specs file

---
+++ src/specs/KeyboardSpecs.js
@@ -0,0 +1,7 @@
+import assert from "assert"
+import Keyboard from "./../Pages/components/keyboard/keyboard.jsx"
+
+describe("Water Bottle", function(){
+  // it("should be empty at start", function(){
+  // });
+});
