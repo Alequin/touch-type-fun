d101fb2d62178036f3fef9f68600ff42a805e296
James Cox <j-cox03@hotmail.com>
2017-10-27 09:01:55 +0100

    made a key responsive to press of a and shift press

--- src/Pages/components/keyboard/index.jsx
+++ src/Pages/components/keyboard/index.jsx
@@ -2,6 +2,7 @@
 import React from 'react'
 
 import Key from './../key'
+import KeyboardHelper from './KeyboardHelper'
 
 class Keyboard extends React.Component {
 
@@ -9,28 +10,49 @@ class Keyboard extends React.Component {
     super(props)
     this.setEventListener()
     this.state = {
-      a: false
+      letters: KeyboardHelper.getLowerCaseLetters(),
+      pressed: {
+        Shift: false,
+        a: false
+      }
     }
   }
 
   setEventListener(){
     document.addEventListener('keydown', (event) => {
-      const letterState = {}
-      letterState[event.key] = true
-      this.setState(letterState)
+      const key = event.key
+      let newState = Object.assign({}, this.state)
+      if(key === "Shift") this.onShiftDown(newState)
+      newState.pressed[key] = true
+      this.setState(newState)
     });
 
     document.addEventListener('keyup', (event) => {
-      const letterState = {}
-      letterState[event.key] = false
-      this.setState(letterState)
+      const key = event.key
+      const newState = Object.assign({}, this.state)
+      if(key === "Shift") this.onShiftUp(newState)
+      newState.pressed[key] = false
+      this.setState(newState)
     });
   }
 
+  onShiftDown(state){
+    state.letters = KeyboardHelper.getUpperCaseLetters()
+    state.pressed["Shift"] = true
+    this.setState(state)
+  }
+
+  onShiftUp(state){
+    state.letters = KeyboardHelper.getLowerCaseLetters()
+    state.pressed["Shift"] = false
+    this.setState(state)
+  }
+
   render() {
+
     return (
       <div className="keyboard-container">
-        <Key highlight={this.state["a"]} letter="a"/>
+        <Key highlight={this.state.pressed["a"]} letter={this.state.letters["a"]}/>
       </div>
     )
   }
