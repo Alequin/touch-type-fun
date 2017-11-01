ce26008b8018fd86899f5124c49b0c8e71e18ada
James Cox <j-cox03@hotmail.com>
2017-11-01 20:44:41 +0000

    Made method to prepare inital state on menuBar

--- src/components/menuBar/Index.jsx
+++ src/components/menuBar/Index.jsx
@@ -6,12 +6,25 @@ import StringHelper from "./../../util/StringHelper"
 
 class MenuBar extends React.Component {
 
+  constructor(props){
+    super(props)
+    this.state = this.buildInitalState(this.props.options)
+  }
+
+  buildInitalState(options){
+    const state = {}
+    for(let index in options){
+      state[options[index]] = index === 0
+    }
+    return state
+  }
+
   renderMenuOptions(){
     const options = []
     let key = 0
     for(let option of this.props.options){
       const text = StringHelper.capitalise(option)
-      options.push(<MenuOption key={key++} text={text} />)
+      options.push(<MenuOption key={key++} text={text} highlighted={this.state.option}/>)
     }
     return options
   }
