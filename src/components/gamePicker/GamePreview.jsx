859b18cc4cb39b342ad160a0ed2d679d8c09c01c
James Cox <j-cox03@hotmail.com>
2017-11-02 19:19:21 +0000

    made shell of game preview

---
+++ src/components/gamePicker/GamePreview.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import css from "./GamePicker.scss"
+
+class GamePreview extends React.Component{
+
+  constructor(props){
+    super(props)
+  }
+
+  render(){
+    return (
+      <div className="game-preview-container">
+        
+      </div>
+    )
+  }
+
+}
+export default GamePreview;
