328cd6c4a7cd9f0bc40eccadb264cba64654d571
James Cox <j-cox03@hotmail.com>
2017-11-02 09:10:01 +0000

    SelectorView: removed method renderGamePicker and replaced with a single game picker component

---
+++ src/components/gamePicker/Index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class GamePicker extends React.Component{
+
+  render(){
+    return (
+      <div className="game-picker-container"></div>
+    )
+  }
+
+}
+export default GamePicker;
