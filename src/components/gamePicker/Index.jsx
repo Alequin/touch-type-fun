f9b83fe71d49fda6b66f2a5809c73c7001b66d14
James Cox <j-cox03@hotmail.com>
2017-11-02 09:20:14 +0000

    Styled the three section of the game picker

--- src/components/gamePicker/Index.jsx
+++ src/components/gamePicker/Index.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 
+import css from "./GamePicker.scss"
+
 class GamePicker extends React.Component{
 
   render(){
     return (
-      <div className="game-picker-container"></div>
+      <div className="game-picker-container">
+        <div className="arrow-container left-arrow"></div>
+        <div className="games"></div>
+        <div className="arrow-container right-arrow"></div>
+      </div>
     )
   }
 
