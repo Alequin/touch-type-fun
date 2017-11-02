585ce0339b4e0a2b56c0a5b218b2f4d84c275012
James Cox <j-cox03@hotmail.com>
2017-11-02 09:45:27 +0000

    added in arrows to game picker

--- src/components/gamePicker/Index.jsx
+++ src/components/gamePicker/Index.jsx
@@ -7,9 +7,13 @@ class GamePicker extends React.Component{
   render(){
     return (
       <div className="game-picker-container">
-        <div className="arrow-container left-arrow"></div>
+        <div className="arrow-container left-arrow">
+          <img src="static/games/images/arrow_small_left.png" alt="arrow left"/>
+        </div>
         <div className="games"></div>
-        <div className="arrow-container right-arrow"></div>
+        <div className="arrow-container right-arrow">
+          <img src="static/games/images/arrow_small_right.png" alt="arrow right"/>
+        </div>
       </div>
     )
   }
