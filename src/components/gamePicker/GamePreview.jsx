0c8100dcc0d6a962ad72a61f5212388f71804117
James Cox <j-cox03@hotmail.com>
2017-11-02 20:16:58 +0000

    Passed requested game data into game preview

--- src/components/gamePicker/GamePreview.jsx
+++ src/components/gamePicker/GamePreview.jsx
@@ -10,11 +10,8 @@ class GamePreview extends React.Component{
   render(){
     return (
       <div className="game-preview-container" style={{"width": this.props.width}}>
-        <h4>Title</h4>
-        <p>
-          Descripton: Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        </p>
+        <h4>{this.props.title}</h4>
+        <p>{this.props.description}</p>
         <div className="score-frame">
           <p>Best Time: 00:00</p>
           <p>Average Time: 00:00</p>
