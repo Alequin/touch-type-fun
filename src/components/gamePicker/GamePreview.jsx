7932c1cc1e931b42b6e29c8d3e20a90db1dc1a92
James Cox <j-cox03@hotmail.com>
2017-11-02 21:26:34 +0000

    Set description on member preview to fixed height

--- src/components/gamePicker/GamePreview.jsx
+++ src/components/gamePicker/GamePreview.jsx
@@ -11,7 +11,7 @@ class GamePreview extends React.Component{
     return (
       <div className="game-preview-container" style={{"width": this.props.width}}>
         <h4>{this.props.game.title}</h4>
-        <p>{this.props.game.description}</p>
+        <p className="description">{this.props.game.description}</p>
         <div className="score-frame">
           <p>Best Time: {this.props.game.topScore()} Seconds</p>
           <p>Average Time: {this.props.game.averageScore()} Seconds</p>
